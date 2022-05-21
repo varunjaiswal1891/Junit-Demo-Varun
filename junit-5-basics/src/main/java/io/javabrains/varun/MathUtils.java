47bc5319e30cdfeec4780eb6c76b7b421f5c8f8b
Varun Jaiswal <varjaisw@cisco.com>
2022-05-21 10:01:38 +0530

    First commit for junit 5 demo on 21 May 2022

---
+++ junit-5-basics/src/main/java/io/javabrains/varun/MathUtils.java
@@ -0,0 +1,9 @@
+package io.javabrains.varun;
+
+public class MathUtils {
+	
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
+}
---
+++ junit-5-basics/src/test/java/io/javabrains/varun/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.javabrains.varun;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("This test ran by varun");
+	}
+
+}
