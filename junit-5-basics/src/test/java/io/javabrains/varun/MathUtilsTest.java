598c6f0b326879a6e13392412710407c3989f2f4
Varun Jaiswal <varjaisw@cisco.com>
2022-06-10 22:53:48 +0530

    Assert annotation demo 10 June 2022

--- junit-5-basics/src/test/java/io/javabrains/varun/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/varun/MathUtilsTest.java
@@ -8,7 +8,11 @@ class MathUtilsTest {
 	@Test
 	void test() {
 		//fail("Not yet implemented");
-		System.out.println("This test ran by varun");
+		//System.out.println("This test ran by varun");
+		MathUtils mathUtils = new MathUtils();
+		int expected =4;
+		int actual = mathUtils.add(1,44);
+		assertEquals(expected,actual,"The add method should add two numbers");
 	}
 
 }
