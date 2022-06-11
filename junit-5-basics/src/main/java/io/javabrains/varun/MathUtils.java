fb49f78df7184b48d1625aca25126f30ed75b4a1
Varun Jaiswal <varjaisw@cisco.com>
2022-06-11 20:29:54 +0530

    Changes for various annotations on 11 june 2022

--- junit-5-basics/src/main/java/io/javabrains/varun/MathUtils.java
+++ junit-5-basics/src/main/java/io/javabrains/varun/MathUtils.java
@@ -6,4 +6,21 @@ public class MathUtils {
 	{
 		return a + b;
 	}
+	public int subtract(int a, int b)
+	{
+		return a - b;
+	}
+	public int multiply(int a, int b)
+	{
+		return a * b;
+	}
+	public int divide(int a, int b)
+	{
+		return a / b;
+	}
+	
+	public double computeCircleArea(double radius)
+	{
+		return 3.14 * radius * radius;
+	}
 }
--- junit-5-basics/src/test/java/io/javabrains/varun/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/varun/MathUtilsTest.java
@@ -1,18 +1,93 @@
 package io.javabrains.varun;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
+	
+	MathUtils mathUtils;
+	
+	@BeforeAll
+    void beforeAllInit() {
+		System.out.println("this needs to run before all ");
+	}
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+	
+	@AfterEach
+	void cleanup() {
+		System.out.println("Cleaning up...");
+	}
 
 	@Test
-	void test() {
+	void testAddPositive() {
 		//fail("Not yet implemented");
 		//System.out.println("This test ran by varun");
-		MathUtils mathUtils = new MathUtils();
+		//MathUtils mathUtils = new MathUtils();
 		int expected =4;
-		int actual = mathUtils.add(1,44);
+		int actual = mathUtils.add(1,3);
 		assertEquals(expected,actual,"The add method should add two numbers");
+	}
+	
+	
+	@Test
+	@DisplayName("multiply method")
+	void testMultiply() {
+		
+		//assertEquals(12,mathUtils.multiply(4, 3),"Method should give product of two numbers");
+
+		assertAll(
+				() -> assertEquals(12,mathUtils.multiply(4, 3)),
+				() -> assertEquals(0,mathUtils.multiply(0, 4)),
+				() -> assertEquals(-2,mathUtils.multiply(2,-1))
+				);
+	}
+	
+	
+	@Test
+	@EnabledOnOs(OS.MAC)
+	@DisplayName("Test Divide method")
+	void testDivide() {
+		
+		//MathUtils mathUtils = new MathUtils();
+		
+		boolean isServerUp = true;
+		//value boolean decides whether this test should run or not eg - no need to run this test if server is down
+		assumeTrue(isServerUp);
+		
+		assertThrows(ArithmeticException.class, () -> {mathUtils.divide(2, 0);},"Method to test divide by zero to throw");
+		//assertThrows(NullPointerException.class, () -> {mathUtils.divide(2, 0);},"Method to test divide by zero to throw");
+	}
+	
+	@Test
+	void testComputeCircleRadius() {
+		//MathUtils mathUtils = new MathUtils();
+		assertEquals(314,mathUtils.computeCircleArea(10),"This should find circle area");
+	}
+	
+	@Test
+	@Disabled
+	@DisplayName("TDD Method. Should not run")
+	void testDisabled()
+	{
+		fail("This test should be disabled");
 	}
 
 }
